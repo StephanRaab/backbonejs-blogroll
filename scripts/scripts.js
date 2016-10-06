0a6a50292129d0fa4fd65c596459335af20457bf
Stephan Raab <stephan.raab@nyhart.com>
2016-10-06 18:00:20 -0400

    stumbling through tutorial part 3/6

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -20,6 +20,35 @@ var BlogView = Backbone.View.extend({
     initialize: function() {
         this.template = _.template($(".blogs-list-template").html());
     },
+    events: {
+        'click .editBlog': 'edit',
+        'click .updateBlog': 'update'
+    },
+    edit: function() {
+        $(".editBlog").toggleClass('hidden');
+        $(".deleteBlog").toggleClass('hidden');
+        $(".updateBlog").toggleClass('hidden');
+        $(".cancelUpdate").toggleClass('hidden');
+
+        var author = this.$('.author').html();
+        var title = this.$('.title').html();
+        var url = this.$('.url').html();
+
+        //author edit input
+        this.$('.author').html('<input type="text" class="form-control authorInput" value="' + author + '"></input>');
+
+        //title edit input
+        this.$('.title').html('<input type="text" class="form-control titleInput" value="' + title + '"></input>');
+
+        //url edit input
+        this.$('.url').html('<input type="text" class="form-control urlInput" value="' + url + '"></input>');
+    },
+    update: function(){
+        //set values from the edited input boxes to be the new values
+        this.model.set('author', $('.authorInput').val());
+        this.model.set('title', $('.titleInput').val());
+        this.model.set('url', $('.urlInput').val());
+    },
     render: function() {
         this.$el.html(this.template(this.model.toJSON()));
         return this;
@@ -31,7 +60,13 @@ var BlogViews = Backbone.View.extend({
     model: blogs,
     el: $('.blogs-list'),
     initialize: function() {
+        var self = this;
         this.model.on('add', this.render, this);
+        this.model.on('change', function(){
+            setTimeout(function(){
+                self.render();
+            }, 50);
+        }, this);
     },
     render: function() {
         var self =  this;
